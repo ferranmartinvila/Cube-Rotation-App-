7d8390fb966031e75aec9da7012c2e1c0caa5cb8
HeladodePistacho <th_sola@hotmail.com>
2017-01-04 14:30:04 +0100

    Quaternions update // Euler axis,angle change the Euler angles panels

--- quat2rotm.m
+++ quat2rotm.m
@@ -3,9 +3,14 @@ function [ matrix ] = quat2rotm( quat )
 %Calculate rotation matrix from quaternion
 if(length(quat(1,:)) == 1 && length(quat(:,1)) == 4)
     
-    q_vect = quat(2:4);
+    if(norm(quat(2:4)) == 0)
+          matrix = [1,0,0;0,1,0;0,0,1];
+    else
+        q_vect = quat(2:4);
     q_x_mat = [0,-q_vect(3),q_vect(2);q_vect(3),0,-q_vect(1);-q_vect(2),q_vect(1),0];
     matrix = ((quat(1)*quat(1)) - (q_vect'*q_vect)) * eye(3) + 2*(q_vect*q_vect') + 2*quat(1)*q_x_mat;
+    
+    end
     
 else
     
